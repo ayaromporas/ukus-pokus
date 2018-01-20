2632f954909c4c84341c9cf034eae9cb86a33dfc
rodjen4jula <bvatovec@gmail.com>
2018-01-20 02:08:38 +0100

    paginavija v1.1

--- views/category/index.php
+++ views/category/index.php
@@ -7,7 +7,7 @@ $catId = $categorymodel->getId();
 //upit za dobijanje recepata iz konkretne kategorije
 
 $query = " AND (recipe_cats like '%," .  $catId . ",%')";
-$recipesAll = $queryInstance->allRows("recipes",$query);
+$recipesAll = $queryInstance->allRows("recipe_id,recipe_title","recipes",$query);
 
 //upit za dobijanje detalja kategorije
 $query = "cat_id=" . $catId;
@@ -31,11 +31,24 @@ $catName = $queryInstance->singleRow("categories",$query);
 				$numberRecipes = count($recipesAll);
 				?>
 			 <div class='col-sm-12 col-xl-12 text-center'>
-			 <h4>Ukupan broj recepata u ovoj kategoriji je :<span style='color: #28a745 !important; font-size: 2rem;'> <?php echo $numberRecipes ?></span></h4>
+			 <h4>Ukupan broj recepata u ovoj kategoriji je :<span style='color: red !important; font-size: 2rem;'> <?php echo $numberRecipes ?></span></h4>
 			 </div><br>
 			 <?php
+
+			 if(isset($_POST['page']) && ($_POST['page'] !=null && $_POST['page'] !="") ) {
+       			$page = $_POST['page'];
+
+       			$start = ($page-1)*12;
+       			     			
+       	  	  }else {
+       			$start = 0;
+       			$page =1;
+       		 }
+       			$end = 12; 
+
+      	$recRowsSliced = array_slice($recipesAll,$start,$end);
 			// ispis liste recepata
-			foreach ($recipesAll as $recipe) {
+			foreach ($recRowsSliced as $recipe) {
 				$recipeId= mb_strtolower($recipe['recipe_id']." ".$recipe['recipe_title'], 'UTF-8');
           		$recipeId = str_replace(" ", "-", $recipeId);
           		$recipeId = $queryInstance->convertExtendedToNormal($recipeId);
@@ -56,23 +69,66 @@ $catName = $queryInstance->singleRow("categories",$query);
 			?>
 				<div class='col-sm-12 col-xl-12 text-center'>
 	       	    <ul class='pagination pagination-sm justify-content-center'>";
-	       	    	<br><p>Ovde dođe paginacija... 1 ... 5 6 7 8 9 10 11 ... 128</p><br>
-	       		<!-- echo printPagination($numberRecipes, $page); -->
+	       		<?php echo printPagination($numberRecipes, $page); ?>
 	       		</ul>
 	       		</div><br>
 		
 	</div>
 
-
-
-
-
-
-
-
-
-
-
 </div> <!-- end container fluid -->
+
+<?php
+function printPagination($numberRecipes, $page){
+       			
+					$returnPagination="";
+       				
+       				$pages =ceil($numberRecipes / 12);
+
+       			if ($page > 1) { 
+       			
+				$returnPagination .=  "<a class='page-link' style='cursor:pointer;' id ='1'> Prva </a>";
+			    $returnPagination .=  "<a class='page-link' title='Back' style='cursor:pointer;' id ='".($page-1)."'> < </a>";
+			 	if ($page > 5) { 
+			 	$returnPagination .=  "<a class='page-link' id='point'> ... </a>";}
+					}
+				if ($page>3) {
+							if ($page>($pages-3)){
+								$startt = $page - 3;
+						 		$endd = $pages;
+							} else {
+								$startt = $page - 3;
+						 		$endd = $page + 3;
+							}	
+						 } else {
+						 	if ($pages >= 7){
+						 	$startt = 1;
+						 	$endd = 7;
+						 }else {
+						 	$startt = 1;
+						 	$endd = $pages;
+						 }
+						 }
+
+       			for ($pageid=$startt; $pageid<=$endd; $pageid++) {
+
+       			if ($pageid == $page){
+       				$returnPagination .=  "<a class='page-link' style='color: red; font-weight:700;cursor:pointer;' id ='".$pageid."'>" . $pageid . "</a>";
+       			}else {
+			
+				$returnPagination .=  "<a class='page-link' style='cursor:pointer;' id ='".$pageid."'>" . $pageid . "</a>";
+				}
+			
+		 
+				}
+
+
+				if ($page< $pages) { 
+				  if ($page < $pages - 3) { $returnPagination .=  "<a class='page-link' id='pointa'> ... </a>";}
+				 $returnPagination .=  "<a class='page-link' style='cursor:pointer;' title='Next' id ='".($page+1)."'> > </a>";
+				 $returnPagination .=  "<a class='page-link' style='cursor:pointer;' id ='".$pages."' > Poslednja <span class='badge badge-warning text-center' id ='".$pages."'>". $pages ."</span></a>";
+		}
+
+			return $returnPagination;
+       			}
 
 

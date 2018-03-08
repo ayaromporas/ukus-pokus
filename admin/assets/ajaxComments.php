35e378223ef42381813f06b0a8ca41eb00105e76
rodjen4jula <bvatovec@gmail.com>
2018-03-09 00:20:36 +0100

    left/inner join

--- admin/assets/ajaxComments.php
+++ admin/assets/ajaxComments.php
@@ -27,7 +27,7 @@ if(isset($_POST['approvedChoice']) && $_POST['approvedChoice']!==null && $_POST[
 		$database->bind(':id', $recipeId);
 		$database->execute();
 
-		$database->query('SELECT comments.comment_id, comments.comment_name, comments.comment_email, comments.comment_text, comments.comment_time, comments.status, recipes.recipe_id, recipes.recipe_permalink, recipes.recipe_title FROM comments LEFT JOIN recipes ON comments.recipe_id = recipes.recipe_id  WHERE comments.status=2 ORDER BY comments.comment_time ASC');
+		$database->query('SELECT comments.comment_id, comments.comment_name, comments.comment_email, comments.comment_text, comments.comment_time, comments.status, recipes.recipe_id, recipes.recipe_permalink, recipes.recipe_title FROM comments INNER JOIN recipes ON comments.recipe_id = recipes.recipe_id  WHERE comments.status=2 ORDER BY comments.comment_time ASC');
 
 	 $comments = $database->resultSet();
    $numberComments = count($comments);
@@ -115,7 +115,7 @@ if(isset($_POST['approvedChoice']) && $_POST['approvedChoice']!==null && $_POST[
 
   } else {
 
- $database->query('SELECT comments.comment_id, comments.comment_name, comments.comment_email, comments.comment_text, comments.comment_time, comments.status, recipes.recipe_id, recipes.recipe_title, recipes.recipe_permalink FROM comments LEFT JOIN recipes ON comments.recipe_id = recipes.recipe_id  WHERE comments.status=2 ORDER BY comments.comment_time ASC');
+ $database->query('SELECT comments.comment_id, comments.comment_name, comments.comment_email, comments.comment_text, comments.comment_time, comments.status, recipes.recipe_id, recipes.recipe_title, recipes.recipe_permalink FROM comments INNER JOIN recipes ON comments.recipe_id = recipes.recipe_id  WHERE comments.status=2 ORDER BY comments.comment_time ASC');
 
 $comments = $database->resultSet();
 $numberComments = count($comments);

b5e093617c0153a4a20577e061849d25d7a75491
Aya Romporas <aya.romporas@gmail.com>
2018-03-23 12:18:41 +0100

    unos slika aya 23.3.

--- admin/views/Images/insert.php
+++ admin/views/Images/insert.php
@@ -6,23 +6,23 @@ if(($superadmin || $admin || $editor) === true){
 <div class="box-typical box-typical-padding">				
 	<h5 class="m-t-lg with-border text-center"><i class="font-icon color-blue far fa-images"></i>&nbsp;&nbsp;&nbsp;<strong>Unos nove slike</strong>&nbsp;&nbsp;&nbsp;<i class="font-icon color-blue far fa-images"></i></h5><br>
 
-	<form method="POST" action="<?php echo ROOT_URL; ?>images/insert" name="newphoto">
+	<form method="POST" action="<?php echo ROOT_URL; ?>images/insert" name="newphoto" enctype="multipart/form-data" >
 		<div class="form-group row">
 			<label class="col-sm-2 form-control-label">Naziv</label>
 			<div class="col-sm-9">
-				<p class="form-control-static"><input type="text" class="form-control" id="photoname" placeholder='Početno slovo mora biti malo, npr. "američke palačinke"... ' name="photoname" required="required"></p>
+				<p class="form-control-static"><input type="text" class="form-control" id="photoname" placeholder='Početno slovo mora biti malo, npr. "američke palačinke"... ' name="photoname"></p>
 			</div>
 		</div>
 		<div class="form-group row">
 			<label class="col-sm-2 form-control-label">Alt</label>
 			<div class="col-sm-9">
-				<p class="form-control-static"><input type="text" class="form-control" id="photoalt" placeholder='Početno slovo mora biti malo, npr. "američke palačinke"... ' name="photoalt" required="required"></p>
+				<p class="form-control-static"><input type="text" class="form-control" id="photoalt" placeholder='Početno slovo mora biti malo, npr. "američke palačinke"... ' name="photoalt" ></p>
 			</div>
 		</div>
 		<div class="form-group row">
 			<label class="col-sm-2 form-control-label">Link</label>
 			<div class="col-sm-9">
-				<p class="form-control-static"><input type="text" class="form-control" id="photolink" name="photolink" placeholder='Samo mala slova, brojeve, crtice "-" i tačku ".", npr. "americke-palacinke.jpg"... ' required="required"></p>
+				<p class="form-control-static"><input type="text" class="form-control" id="photolink" name="photolink" placeholder='Samo mala slova, brojeve, crtice "-" i tačku ".", npr. "americke-palacinke.jpg"... ' ></p>
 			</div>
 		</div>
 		<div class="form-group row">
@@ -31,6 +31,15 @@ if(($superadmin || $admin || $editor) === true){
 				<p class="form-control-static"><input type="text" class="form-control" id="photorecid" name="photorecid" placeholder='Isključivo brojevi, u obliku npr. "25"...' ></p>
 			</div>
 		</div>
+
+		<div class="form-group row">
+			<label class="col-sm-2 form-control-label">Upload slike</label>
+			<div class="col-sm-9">			
+		                       	<p class="form-control-static"><input type="file" id="fileToUpload" name="fileToUpload" placeholder="Izaberite sliku"></p>			
+		             </div>
+
+		                </div>
+		            </div>
 	
 		<div class="text-center">
 			<button type="text" name="submit" value="submit" class="btn btn-rounded btn-success">Sačuvaj</button>
@@ -41,6 +50,14 @@ if(($superadmin || $admin || $editor) === true){
 </div><!--.box-typical-->
 
 
+<?php 
+
+if(isset($_POST['submit'])){
+	var_dump($_POST);
+	var_dump($_FILES);
+}
+
+ ?>
 
 
 
